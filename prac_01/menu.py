fd2fd389644a5f53f3e1a216b3d49dc186a35510
lbw138286 <yuzhe.fu@my.jcu.edu.au>
2025-09-22 13:46:19 +0800

    week 2

---
+++ prac_01/menu.py
@@ -0,0 +1,24 @@
+"""
+get name
+get choice
+    while choice != 'Q'
+        if choice == 'H'
+            print(f"Hello {name}")
+        else if choice == 'G'
+            print(f"Goodbye {name}")
+        else
+            print("Invalid choice")
+        get choice
+    print("Finished.")
+"""
+name = input("Enter name: ")
+choice = input("(H)ello\n(G)oodbye\n(Q)uit\n>>> ").upper()
+while choice != "Q":
+    if choice == "H":
+        print(f"Hello {name}")
+    elif choice == "G":
+        print(f"Goodbye {name}")
+    else:
+        print("Invalid choice")
+    choice = input("(H)ello\n(G)oodbye\n(Q)uit\n>>> ").upper()
+print("Finished.")
